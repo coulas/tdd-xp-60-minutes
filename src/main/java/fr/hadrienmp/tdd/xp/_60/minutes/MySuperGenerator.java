ad344f88bcea311c5af1830f74ed320205929092
DarzuL <guillaumebourderye@gmail.com>
2017-11-10 14:21:54 +0100

    generate even number

---
+++ src/main/java/fr/hadrienmp/tdd/xp/_60/minutes/MySuperGenerator.java
@@ -0,0 +1,8 @@
+package fr.hadrienmp.tdd.xp._60.minutes;
+
+class MySuperGenerator {
+
+    public int givenEvenNumber() {
+        return 2;
+    }
+}
--- src/test/java/fr/hadrienmp/tdd/xp/_60/minutes/MyTest.java
+++ src/test/java/fr/hadrienmp/tdd/xp/_60/minutes/MyTest.java
@@ -2,10 +2,18 @@ package fr.hadrienmp.tdd.xp._60.minutes;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertTrue;
+
 public class MyTest {
 
-    @Test
-    public void myTest() {
+    private MySuperGenerator SUT = new MySuperGenerator();
 
+    @Test
+    public void givenAnEvenNumber() {
+        assertNumberIsEven(SUT.givenEvenNumber());
+    }
+
+    private void assertNumberIsEven(int number) {
+        assertTrue(number % 2 == 0);
     }
 }
